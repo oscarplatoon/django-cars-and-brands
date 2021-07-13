be358843ae105d2d94aed05d5873d6a5b3b78f6d
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-07-13 16:58:52 -0500

    Shawn and Don's Cars and Brands Solution - implemented Brands List, Edit, New

---
+++ car_app/cars_and_brands/urls.py
@@ -0,0 +1,19 @@
+# /brands # a list of all the car brands
+# /brands/new # form for a new car brand
+# /brands/<:id> # see a specific car brand
+# /brands/<:id>/edit # edit page for a specific car brand
+
+# /brands/<:brand_id>/cars # a list of cars for a specific car brand
+# /brands/<:brand_id>/cars/new # form for a new car under a specific car brand
+# /brands/<:brand_id>/cars/<:car_id> # see a specific car for a specific car brand
+# /brands/<:brand_id>/cars/<:car_id>/edit # edit page for a specific car under a specific car brand
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.brands_list, name='brands_list'),
+    path('new', views.new_brand, name='new_brand'),
+    # path('<int:brand_id>', views.brand_details, name='brand_details'),
+    path('<int:brand_id>/edit', views.edit_brand, name='edit_brand'),
+]
--- car_app/cars_and_brands/views.py
+++ car_app/cars_and_brands/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Brand, Car
+from .forms import BrandForm
 
-# Create your views here.
+def brands_list(request):
+    brands = Brand.objects.all()
+    return render(request, 'brands/brands_list.html', {'brands': brands})
+
+def new_brand(request):
+    if request.method =="POST":
+        form = BrandForm(request.POST)
+        if form.is_valid():
+            brand = form.save(commit=False)
+            brand.save()
+            return redirect('/brands')
+    else:
+        form = BrandForm()
+    
+    return render(request, 'brands/brand_form.html', {'form': form, 'type_of_request': 'New'})
+
+
+def edit_brand(request, brand_id):
+    brand = Brand.objects.get(id=brand_id)
+    if request.method =="POST":
+        form = BrandForm(request.POST, instance=brand)
+        if form.is_valid():
+            brand = form.save(commit=False)
+            brand.save()
+            return redirect('/brands')
+    else:
+        form = BrandForm(instance=brand)
+    
+    return render(request, 'brands/brand_form.html', {'form': form, 'type_of_request': 'Edit'})
