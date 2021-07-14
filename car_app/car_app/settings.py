ef2f5eee6c009eeed5249472f9a34743d242febe
RenoAverill <reno_averill@yahoo.com>
2021-07-13 19:37:34 -0600

    Renos incomplete cars and brands

--- car_app/car_app/settings.py
+++ car_app/car_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cars_and_brands'
+    'cars_and_brands',
 ]
 
 MIDDLEWARE = [
--- car_app/cars_and_brands/views.py
+++ car_app/cars_and_brands/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Brand, Car
 
 # Create your views here.
+# # urlpatterns = [
+#     path('', views.brand_list, name='brand_list'),
+#     path('<int:brand_id>', views.model_list, name='model_list'),
+#     path('<int:brand_id>/add_brand', views.add_brand, name='add_brand'),
+#     path('<int:brand_id>/delete_brand', views.delete_brand, name='delete_brand'),
+#     path('<int:brand_id>/edit_car', views.edit_car, name='edit_car'),
+# ]
+
+
+def brand_list(request):
+    brands = Brand.objects.all()
+    return render(request, )
